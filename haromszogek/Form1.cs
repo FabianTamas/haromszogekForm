e02b17e66700b3008ad285c097587b53a2627347
FabianTamas <fabiant0130@gmail.com>
2020-10-05 13:13:50 +0200

    Fájl megnyitás elkezdése

--- haromszogek/Form1.cs
+++ haromszogek/Form1.cs
@@ -82,5 +82,11 @@ namespace haromszogek
                 MessageBox.Show("Nincs mit törölni!");
             }
         }
+
+        private void btnFajlbol_Click(object sender, EventArgs e)
+        {
+            lbHaromszogLista.Items.Clear();
+            ofdMegnyitas.ShowDialog();
+        }
     }
 }
